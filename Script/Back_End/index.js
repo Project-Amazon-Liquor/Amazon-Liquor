d39ba8a6ecdfa14843bae632e28aca84986dff80
Stephen Yeboah <stephenoyeboah@gmail.com>
2021-07-21 19:13:51 -0700

    updated the crud

--- Script/Back_End/index.js
+++ Script/Back_End/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoClient = require("mongodb").MongoClient;
+const e = require("express");
 //const { response } = require("express");
 //const { request, response } = require("express");
 const connectionString =
@@ -50,8 +51,47 @@ mongoClient
     //create new row(s)
     //add new product
     server.post("/products", (request, response) => {
+      let {
+        _id,
+        brand,
+        volume,
+        alcohol_percentage,
+        category,
+        retail_price,
+        url,
+      } = request.body;
+      if (_id === undefined) {
+        return response.status(400).send("_id is required");
+      }
+      if (brand === undefined) {
+        return response.status(400).send("brand is required");
+      }
+      if (volume === undefined) {
+        return response.status(400).send("volume is required");
+      }
+      if (alcohol_percentage === undefined) {
+        return response.status(400).send("alcohol percentage is required");
+      }
+      if (category === undefined) {
+        return response.status(400).send("category is required");
+      }
+      if (retail_price === undefined) {
+        return response.status(400).send("retail price is required");
+      }
+      if (url === undefined) {
+        return response.status(400).send("url is required");
+      }
+      let insertRecord = {
+        _id: parseInt(_id),
+        Brand: brand,
+        Volume: volume,
+        Alcohol_Percentage: alcohol_percentage,
+        Category: category,
+        Retail_Price: retail_price,
+        Url: url,
+      };
       productTable
-        .insertOne(request.body)
+        .insertOne(insertRecord)
         .then((status) => {
           response.send({ message: "success" });
         })
@@ -67,8 +107,34 @@ mongoClient
     });
 
     server.post("/customers", (request, response) => {
+      //deconstructe the request body
+      let { _id, first_name, last_name, email, phone, address } = request.body;
+      if (_id === undefined) {
+        return response.status(400).send("_id is required");
+      }
+      if (first_name === undefined) {
+        return response.status(400).send("first_name is required");
+      }
+      if (last_name === undefined) {
+        return response.status(400).send("last_name is required");
+      }
+      if (email === undefined) {
+        return response.status(400).send("email is required");
+      }
+      let customerRecord = {
+        _id: parseInt(_id),
+        First_Name: first_name,
+        Last_Name: last_name,
+        Email: email,
+      };
+      if (phone !== undefined) {
+        customerRecord["Phone"] = phone;
+      }
+      if (address !== undefined) {
+        customerRecord["Address"] = address;
+      }
       customerTable
-        .insertOne(request.body)
+        .insertOne(customerRecord)
         .then((status) => response.send({ message: "success" }))
         .catch((err) => response.status(400).send(err));
     });
@@ -171,7 +237,6 @@ mongoClient
       let queryParam = {};
       let keyPara = {};
       let _id = request.query._id || request.body._id;
-      console.log(_id);
       let { brand, volume, alcohol_percentage, category, retail_price } =
         request.body;
       if (_id === undefined) {
@@ -195,31 +260,166 @@ mongoClient
       if (retail_price !== undefined) {
         queryParam["Retail_Price"] = retail_price;
       }
-      console.log(queryParam);
-      console.log(keyPara);
+
       productTable
         .updateOne(keyPara, {
           $set: queryParam,
         })
-        .then((data) => response.send(data))
+        .then((data) => {
+          if (data["matchedCount"] > 0) {
+            response.send(`${data["matchedCount"]} record(s) updated`);
+          } else {
+            response.send("No record was updated");
+          }
+        })
         .catch((err) => response.status(404).send(err));
     });
 
     //orders
-    server.put("/orders/update", (request, response) => {});
+    server.put("/orders/update", (request, response) => {
+      let orderParams = {};
+      let keyPara = {};
+      let _id = request.query._id || request.body._id;
+      let { quantity, total_price, order_date } = request.body;
+      if (_id === undefined) {
+        return response.status(400).send("_id is required");
+      }
+      keyPara["_id"] = parseInt(_id);
+      if (quantity !== undefined) {
+        orderParams["Quantity"] = parseInt(quantity);
+      }
+      if (total_price !== undefined) {
+        orderParams["Total_Price"] = parseFloat(total_price);
+      }
+      if (order_date !== undefined) {
+        orderParams["Order_Date"] = order_date;
+      }
+
+      orderTable
+        .updateOne(keyPara, {
+          $set: orderParams,
+        })
+        .then((data) => {
+          if (data["matchedCount"] > 0) {
+            response
+              .status(200)
+              .send(`${data["matchedCount"]} record(s) updated`);
+          } else {
+            response.status(200).send("No record updated");
+          }
+        })
+        .catch((err) => response.status(400).send(err));
+    });
 
     //customers
-    server.put("/customers/update", (request, response) => {});
+    server.put("/customers/update", (request, response) => {
+      let customerParams = {};
+      let keyPara = {};
+      let _id = request.query._id || request.body._id;
+      let { email, phone, address } = request.body;
+      if (_id === undefined) {
+        return response.status(400).send("?_id is required");
+      }
+      keyPara["_id"] = parseInt(_id);
+      if (email !== undefined) {
+        customerParams["Email"] = email;
+      }
+      if (phone !== undefined) {
+        customerParams["Phone"] = phone;
+      }
+      if (address !== undefined) {
+        customerParams["Address"] = address;
+      }
+
+      customerTable
+        .updateOne(keyPara, {
+          $set: customerParams,
+        })
+        .then((data) => {
+          if (data["matchedCount"] > 0) {
+            response
+              .status(200)
+              .send(`${data["matchedCount"]} record(s) updated`);
+          } else {
+            response.status(200).send(`No was record updated`);
+          }
+        })
+        .catch((err) => response.send(err));
+    });
 
     //delete item from the database
     //products
-    server.delete("/products/delete", (request, response) => {});
+    server.delete("/products/delete", (request, response) => {
+      let key = {};
+      let _id = request.body._id || request.query._id;
+      if (_id === undefined) {
+        return response
+          .status(400)
+          .send("_id is required in order to delete a record");
+      }
+      key["_id"] = parseInt(_id);
+      productTable
+        .deleteOne(key)
+        .then((status) => {
+          if (status["deletedCount"] > 0) {
+            response
+              .status(200)
+              .send(`${status["deletedCount"]} record(s) deleted`);
+          } else {
+            response.status(200).send(`No record deleted`);
+          }
+        })
+        .catch((err) => response.send(err));
+    });
 
     //orders
-    server.delete("/orders/delete", (request, response) => {});
+    server.delete("/orders/delete", (request, response) => {
+      let orderKey = {};
+      let _id = request.body._id || request.query._id;
+
+      if (_id === undefined) {
+        return response
+          .status(400)
+          .send("_id is required in order to delete a record");
+      }
+      orderKey["_id"] = parseInt(_id);
+      orderTable
+        .deleteOne(orderKey)
+        .then((status) => {
+          if (status["deletedCount"] > 0) {
+            response
+              .status(200)
+              .send(`${status["deletedCount"]} record(s) deleted`);
+          } else {
+            response.status(200).send(`No record deleted`);
+          }
+        })
+        .catch((err) => response.send(err));
+    });
 
     //customers
-    server.delete("/customers/delete", (request, response) => {});
+    server.delete("/customers/delete", (request, response) => {
+      let customerKey = {};
+      let _id = request.body._id || request.query._id;
+      if (_id === undefined) {
+        return response
+          .status(400)
+          .send("_id is required in order to delete a record");
+      }
+      customerKey["_id"] = parseInt(_id);
+      customerTable
+        .deleteOne(customerKey)
+        .then((status) => {
+          if (status["deletedCount"] > 0) {
+            response
+              .status(200)
+              .send(`${status["deletedCount"]} record(s) deleted`);
+          } else {
+            response.status(200).send(`No record deleted`);
+          }
+        })
+        .catch((err) => response.send(err));
+    });
   })
   .catch((err) => console.error(err));
 const available_port = process.env.PORT || 3000;
