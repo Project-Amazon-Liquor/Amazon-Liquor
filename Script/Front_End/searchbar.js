a9448e3d765e8da564371c5ce0d7f0717c23f6b4
Chasbott182 <chasbott182@gmail.com>
2021-07-22 08:14:44 -0700

    added the search bar to the other pages and fixed the javascript links

--- Script/Front_End/searchbar.js
+++ Script/Front_End/searchbar.js
@@ -1,47 +1,60 @@
-const API_BASE_URL = "";
-const URL =`${API_BASE_URL}/products`;
-const searchBtn = document.getElementById("searchbutton");
+const API_BASE_URL = "https://express-api-cm.herokuapp.com";
+const URL =`${API_BASE_URL}/destinations`;
 
+const bannerAPI = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+
+loadBanner();
 loadFeatureCards();
 
-searchBtn.addEventListener("click", (event)=>{
-
-    event.preventDefault();
-    
-  })
-
-
-  function loadFeatureCards(){
+function loadFeatureCards(){
 
     fetch(URL)
         .then((response)=> response.json())
         .then((features) => {
-            for (let index = 0; index <= 5; index++) {
+            //for (let index = 0; index <= 5; index++) {
+                for (const _id in features){
 
-                let {name, price, photo} = features[index];
+                let {name, location, photo, description} = features[_id];
                 
                 
                 if (name ==="") {
                     name = "Jack Daniel's"; 
                 }
-                if (price ==="") {
-                    price = "9.99"; 
-                }
+
                 if (photo ==="") {
-                    photo = "https://cavchronicle.org/wp-content/uploads/2018/03/top-travel-destination-for-visas-900x504.jpg"; 
+                    photo = ""; 
                 }
 
                 const featureCard = document.createElement("a")
                 featureCard.href = "single-product.html";
                 featureCard.innerHTML = `
                 <div class="featured-item">
-                <img src=${photo} alt="Item ${index}">
+                <img src= ./Image/item-01.jpg alt="Item1">
                     <h4>${name}</h4>
-                    <h6>\"$\"${price}</h6>
+                    <h6>$${location}</h6>
                     </div>
                     `;
-                    document.getElementById("feature-container").appendChild(featureCard);
+                    document.getElementById("featureBanner").appendChild(featureCard);
             }
+        })
+        
+}
+
+  function loadBanner(){
+
+    fetch(bannerAPI)
+        .then((response)=> response.json())
+        .then((banner) => {
+            let strDrinkThumb;             
+            for (const str in banner.drinks[0]) {
+                if(str =="strDrinkThumb"){
+                   strDrinkThumb = banner.drinks[0][str];
+                   break;
+                }
+                
+            }
+            
+           // document.getElementsByClassName("banner").appendChild(strDrinkThumb);
         })
         
 }
