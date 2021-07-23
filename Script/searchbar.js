ac3f41cf9551f08ddb9d7997325d6677ac3baa66
Hongyi <qhyprc@gmail.com>
2021-07-22 22:27:27 -0700

    only need ordersearch

--- Script/searchbar.js
+++ Script/searchbar.js
@@ -1,30 +1,14 @@
-
 const API_BASE_URL = "https://amazonliquor.herokuapp.com";
-const URL =`${API_BASE_URL}/products`;
-
-
+const URL = `${API_BASE_URL}/products`;
 loadFeatureCards();
+function loadFeatureCards() {
+  fetch(URL)
+    .then((response) => response.json())
+    .then((products) => {
+      for (let index = 0; index <= 25; index++) {
+        let { _id, Brand, Retail_Price, URL } = products[index];
 
-function loadFeatureCards(){
-
-    fetch(URL)
-        .then((response)=> response.json())
-        .then((products) => {
-            for (let index = 0; index <= 5; index++) {
-                let {_id, Brand, Retail_Price, URL, } = products[index];
-
-                const featureCard = document.createElement("a")
-                featureCard.href = "single-product.html";
-                featureCard.innerHTML = `
-                <div class="featured-item">
-                <img src= ${URL} alt="Item1">
-                    <h4>${Brand}</h4>
-                    <h6>$${Retail_Price}</h6>
-                    </div>
-                    `;
-
-                    document.getElementById("featureBanner").appendChild(featureCard);
-            }
-        })
-        
+        console.log(_id, Brand, Retail_Price, URL);
+      }
+    });
 }
